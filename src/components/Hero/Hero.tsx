5ff8a3cd2ca5b24eaa0b617462f27ee043db684a
pbareja <pawel.bareja@rst.com.pl>
2022-03-25 12:17:32 +0100

    feat(1553): hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,33 @@
+import Grid from '@mui/material/Grid';
+import Image from 'next/image';
+import theme from '../../../styles/theme';
+
+interface HeroProps {
+  imgSrc?: StaticImageData;
+  imgFit: 'fill' | 'contain' | 'cover' | 'none' | 'scale-down';
+  imgHeight?: number;
+  imgAlt: string;
+}
+
+export const Hero: React.FC<HeroProps> = ({ imgSrc, imgFit, imgHeight, imgAlt }) => {
+  return (
+    <Grid
+      component="section"
+      container
+      sx={{
+        position: `relative`,
+        height: '100vh',
+        ...(imgHeight && {
+          height: `${imgHeight}px`,
+        }),
+        width: `100vw`,
+        background: theme.palette.customGray.main,
+        overflow: `hidden`,
+        zIndex: -100,
+        mb: 15,
+      }}
+    >
+      {imgSrc && <Image src={imgSrc} alt={imgAlt} layout="fill" objectFit={imgFit} />}
+    </Grid>
+  );
+};
