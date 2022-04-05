94833d95e255bff4a1cca78a6b3a071cec392b97
pbareja <pawel.bareja@rst.com.pl>
2022-04-05 16:51:23 +0200

    replace dynamic page fetch with getServerSideProps, add 404 page fallback when fetch fails

--- src/pages/token-detail/[id].tsx
+++ src/pages/token-detail/[id].tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { Grid, Box } from '@mui/material';
 import { ProductCard } from '../../components/ProductCard';
 import { Hero } from '../../components/Hero';
@@ -14,34 +13,17 @@ import { Gallery } from '../../components/Gallery';
 // link to example NFT detail page:
 // http://localhost:3000/token-detail/920d16d7-208f-4955-98c2-f41bee527f08
 
-const DetailPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  // id => token_id = 920d16d7-208f-4955-98c2-f41bee527f08
+type Trait = Record<string, string>;
 
-  const contract_address = '0x54aE5302774dB6F54A52E7B6De1b0a9B3bd94185';
-
-  const [nftData, setData] = useState<any>(null);
-  const [traits, setTraits] = useState<any>(null);
-  const [isLoading, setLoading] = useState(false);
+const DetailPage = ({ nftData }: { nftData: any }) => {
+  const [traits, setTraits] = useState<Trait | null>(null);
 
   useEffect(() => {
-    setLoading(true);
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/token/meta/${contract_address}/${id}.json`)
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((error) => console.log(error.message));
-    setLoading(false);
-  }, [id]);
-
-  useEffect(() => {
-    if (nftData) {
-      const apiTraits: Record<string, string> = {};
-      nftData?.traits?.map((item: Record<string, string>) => {
-        apiTraits[item.trait_type] = item.value;
-      });
-      setTraits(apiTraits);
-    }
+    const apiTraits: Record<string, string> = {};
+    nftData?.traits?.map((item: Record<string, string>) => {
+      apiTraits[item.trait_type] = item.value;
+    });
+    setTraits(apiTraits);
   }, [nftData]);
 
   return (
@@ -49,8 +31,6 @@ const DetailPage = () => {
       <Grid container mt={18.5}>
         <Hero imgSrc={image} imgFit={'cover'} imgHeight={163} imgAlt="alt text" />
       </Grid>
-
-      {isLoading && <p>Loading...</p>}
 
       {nftData && (
         <Box sx={{ maxWidth: 1440, margin: '0 auto', marginTop: '0', padding: '0 8px' }}>
@@ -102,3 +82,24 @@ const DetailPage = () => {
 };
 
 export default DetailPage;
+
+export async function getServerSideProps(context: any) {
+  // id => token_id = 920d16d7-208f-4955-98c2-f41bee527f08
+  const { id } = context.query;
+  const contract_address = '0x54aE5302774dB6F54A52E7B6De1b0a9B3bd94185';
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/token/meta/${contract_address}/${id}.json`,
+  );
+  const data = await response.json();
+
+  if (!data.traits) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { nftData: data },
+  };
+}
