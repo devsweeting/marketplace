84042d515eef7761cfc30efcc4a0aeeb36ec77c8
pbareja <pawel.bareja@rst.com.pl>
2022-05-25 10:26:42 +0200

    wip

---
+++ jest.config.ts
@@ -0,0 +1,70 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/styles/(.*)$': '<rootDir>/styles/$1',
+    '^@/__mocks__/(.*)$': '<rootDir>/src/__mocks__/$1',
+
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.ts',
+
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.ts`,
+  },
+  //  Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+};
+export default config;
+
+// module.exports = {
+//   collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+//   moduleNameMapper: {
+//     // Handle CSS imports (with CSS modules)
+//     // https://jestjs.io/docs/webpack#mocking-css-modules
+//     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+//     // Handle CSS imports (without CSS modules)
+//     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.ts',
+
+//     // Handle image imports
+//     // https://jestjs.io/docs/webpack#handling-static-assets
+//     '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.ts`,
+
+//     // Handle module aliases
+//     '^@/components/(.*)$': '<rootDir>/components/$1',
+//   },
+//   // Add more setup options before each test is run
+//   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+//   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+//   testEnvironment: 'jsdom',
+//   transform: {
+//     // Use babel-jest to transpile tests with the next/babel preset
+//     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+//     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+//   },
+//   transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+//   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+// };
+
+// to działa
+
+// moduleNameMapper: {
+//   '^@/components/(.*)$': '<rootDir>/src/components/$1',
+//   '^@/styles/(.*)$': '<rootDir>/styles/$1',
+// },
