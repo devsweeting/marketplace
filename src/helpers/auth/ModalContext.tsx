4d9bf478e982d40baf4610f8a42f4db713b5a2c0
Frank Krogh <frank@buildmidwestern.com>
2022-06-16 11:05:14 -0500

    open login modal on click if not logged in

--- src/helpers/auth/ModalContext.tsx
+++ src/helpers/auth/ModalContext.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { Dispatch } from 'react';
 import React, { createContext, useCallback, useEffect, useState, useMemo } from 'react';
 
@@ -32,27 +31,6 @@ export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
       setIsOpen,
     }),
     [isOpen, setIsOpen],
-=======
-import React, { createContext, useCallback, useEffect, useState, useMemo } from 'react';
-
-export interface IModalContext {
-  toggle: () => void;
-}
-export const ModalContext = createContext<IModalContext>({
-  toggle: () => false,
-});
-
-export const ModalProvider = ({ children }: { children: React.ReactNode }) => {
-  const [open, setOpen] = useState(false);
-
-  const toggle = useCallback(() => setOpen(!open), [open]);
-
-  const contextValue = useMemo(
-    () => ({
-      toggle,
-    }),
-    [toggle],
->>>>>>> 583b0a9 (Modal Context and provider)
   );
 
   return <ModalContext.Provider value={contextValue}>{children}</ModalContext.Provider>;
