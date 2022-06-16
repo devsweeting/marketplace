874bf6fa3a0d2084a54e66408a3c48cdc7ab4ae3
Frank Krogh <frank@buildmidwestern.com>
2022-06-16 13:47:28 -0500

    rebased with develop

--- src/components/Navbar/components/Login/Login.tsx
+++ src/components/Navbar/components/Login/Login.tsx
@@ -14,6 +14,78 @@ export const Login = () => {
     setIsOpen(!isOpen);
   };
 
+<<<<<<< HEAD
+=======
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailState(event.target.value);
+    setButtonState(false);
+  };
+  const handleClose = () => {
+    setEmailState('');
+    setButtonState(false);
+    setAlertMessage('');
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    //
+  }, [emailState, buttonState, alertMessage]);
+
+  const validate = (email: string) => {
+    if (email.length === 0) {
+      return false;
+    }
+    if (!email.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    setButtonState(true);
+    validate(emailState);
+    if (!validate(emailState)) {
+      setAlertMessage('Please enter a valid email address');
+      if (modalBox.current) {
+        modalBox.current.style.color = '#f44336';
+      }
+      return;
+    }
+
+    const formBody = encodeURIComponent('email') + '=' + encodeURIComponent(emailState);
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users/login/request`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formBody,
+    }).then((res) => {
+      switch (res.status) {
+        case 200:
+          setAlertMessage('Check your email for a link to sign in.');
+          if (modalBox.current) {
+            modalBox.current.style.color = '#4caf50';
+          }
+          setButtonState(true);
+          return;
+        case 429:
+          setAlertMessage('Too many requests. Please try again later.');
+          if (modalBox.current) {
+            modalBox.current.style.color = '#f44336';
+          }
+          setButtonState(true);
+          return;
+        default:
+          if (modalBox.current) {
+            setAlertMessage('Something went wrong. Please try again later.');
+            modalBox.current.style.color = '#ffae00';
+          }
+      }
+    });
+  };
+
+>>>>>>> 3190107 (rebased with develop)
   return (
     <div>
       <a style={{ textDecoration: 'none' }} onClick={handleOpen}>
