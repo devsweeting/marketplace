f072d44aa63c7be7b862a6dd19f8a4bcb2d28627
nb-midwestern <nb@buildmidwestern.com>
2022-06-16 15:09:22 -0600

    Update Gallery.test.tsx

--- src/__tests__/components/Gallery.test.tsx
+++ src/__tests__/components/Gallery.test.tsx
@@ -6,7 +6,7 @@ import { themeJump } from '@/styles/themeJump';
 import { mockGalleryImages } from '@/__mocks__/mockApiData';
 import '@testing-library/jest-dom/extend-expect';
 import { withTestRouter } from '../helpers/TestRouter';
-// import user from '@testing-library/user-event';
+import user from '@testing-library/user-event';
 
 type Image = {
   title: string;
@@ -44,14 +44,12 @@ describe('Gallery', () => {
   test('Gallery image switches on click', async () => {
     render(<MockGallery images={mockGalleryImages} />);
     const images = screen.getAllByRole('img') as HTMLImageElement[];
-    const firstImage = images[0];
-    expect(firstImage).toHaveAttribute('srcs');
 
-    // await images.map(async (image) => {
-    //   await user.click(image);
-    //   const mainImage = screen.getByRole('img') as HTMLImageElement;
-    //   expect(mainImage).toBeVisible();
-    //   expect(mainImage.src).toBe(image.src);
-    // });
+    await images.map(async (image) => {
+      await user.click(image);
+      const mainImage = screen.getByRole('img', { name: 'main-gallery-image' }) as HTMLImageElement;
+      expect(mainImage).toBeVisible();
+      expect(mainImage.src).toBe(image.src);
+    });
   });
 });
