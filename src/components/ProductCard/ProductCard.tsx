a0cdf731585f9c469dcfe8c5348e17b7bb4d45db
Frank Krogh <frank@buildmidwestern.com>
2022-06-16 11:41:11 -0500

    fixed lint errors

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Grid, Typography, Box } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
