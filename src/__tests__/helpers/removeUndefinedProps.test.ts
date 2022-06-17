d0fc99e9b1f0782fb14a254db84747af72cec039
Levi Zitting <me@levizitting.com>
2022-06-17 10:06:17 -0500

    Add test for removeUndefinedProps

--- src/__tests__/helpers/removeUndefinedProps.test.ts
+++ src/__tests__/helpers/removeUndefinedProps.test.ts
@@ -1,11 +1,59 @@
 import { removeUndefinedProps } from '@/helpers/removeUndefinedProps';
+import type { GetServerSideProps, GetServerSidePropsContext } from 'next';
 
-const props = {};
+const mocKContext = {} as GetServerSidePropsContext;
 
-//TODO write tests for removeUndefinedProps
+const mockHandler: GetServerSideProps = async () => {
+  return {
+    props: {
+      undefinedKey: undefined,
+      stringKey: 'string',
+      nestedKey: {
+        undefinedKey: undefined,
+        stringKey: 'string',
+      },
+    },
+  };
+};
+
+const mockHandlerWithAsyncProps: GetServerSideProps = async () => {
+  return {
+    props: Promise.resolve({
+      undefinedKey: undefined,
+      stringKey: 'string',
+      nestedKey: {
+        undefinedKey: undefined,
+        stringKey: 'string',
+      },
+    }),
+  };
+};
 describe('removeUndefinedProps', () => {
-  test('should remove undefined keys', () => {
-    const result = removeUndefinedProps(props);
-    expect(result);
+  test('should remove undefined keys', async () => {
+    const result = await removeUndefinedProps(mockHandler)(mocKContext);
+
+    if (!('props' in result)) {
+      throw new Error('no props in GetServerSideProps result object');
+    }
+
+    expect(result.props).not.toHaveProperty('undefinedKey');
+    expect(result.props).toHaveProperty('stringKey');
+
+    expect(result.props).not.toHaveProperty('nestedKey.undefinedKey');
+    expect(result.props).toHaveProperty('nestedKey.stringKey');
+  });
+
+  test('should remove undefined keys in async props', async () => {
+    const result = await removeUndefinedProps(mockHandlerWithAsyncProps)(mocKContext);
+
+    if (!('props' in result)) {
+      throw new Error('no props in GetServerSideProps result object');
+    }
+
+    expect(result.props).not.toHaveProperty('undefinedKey');
+    expect(result.props).toHaveProperty('stringKey');
+
+    expect(result.props).not.toHaveProperty('nestedKey.undefinedKey');
+    expect(result.props).toHaveProperty('nestedKey.stringKey');
   });
 });
