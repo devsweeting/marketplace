53e58d036c5a8c57ab77e243ad8a6f325b4684fc
Frank Krogh <frank@buildmidwestern.com>
2022-06-21 11:12:19 -0500

    made route to api and changed back login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from 'next';
-import type { ReactNode } from 'react';
-import { useEffect } from 'react';
 import { loginWithToken } from '@/api/endpoints/login';
 import { setUserCookie } from '@/helpers/auth/userCookie';
 import { useLoginPageStyles } from '@/styles/LoginPage.styles';
@@ -9,55 +7,8 @@ import { Box, Container, Typography } from '@mui/material';
 import classNames from 'classnames';
 import { getServerSidePropsWithUser } from '@/helpers/auth/withUser';
 
-const Login: NextPage = (jwt) => {
+const Login: NextPage = () => {
   const classes = useLoginPageStyles();
-  console.log(jwt);
-
-  const getWishList = async () => {
-    if (localStorage.getItem('wishList') && localStorage.getItem('wishList') !== 'undefined') {
-      const wishList = JSON.parse(localStorage.getItem('wishList') as string);
-      return wishList;
-    } else {
-      return [];
-    }
-  };
-
-  useEffect(() => {
-    if (jwt) {
-      const fetchingWatchListToAPI = async (jwt: any) => {
-        console.log('fetchingWatchListToAPI');
-        console.log(jwt);
-        const wishList = await getWishList();
-        if (!wishList || wishList === undefined || wishList.length <= 0) return;
-        for (let i = 0; i < wishList.length; i++) {
-          const item = wishList[i];
-          const response = await fetch(`${process.env.API_URL}/api/wishlist`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${jwt}`,
-            },
-            body: JSON.stringify({
-              itemId: item.itemId,
-            }),
-          });
-          console.log(response);
-          const data = await response.json();
-          console.log(data);
-          return data;
-        }
-      };
-
-      fetchingWatchListToAPI(jwt).then((data) => {
-        if (data) {
-          console.log('Successfully added watchlist to API');
-          console.log(data);
-          localStorage.removeItem('wishList');
-        }
-      });
-    }
-  }, [jwt]);
-
   return (
     <>
       <OpenGraph title={'Login'} description={'Login page description'} />
@@ -104,8 +55,6 @@ export const getServerSideProps = getServerSidePropsWithUser(async ({ req, res, 
         statusCode: 302,
         destination: '/login/success',
       },
-
-      props: { jwt },
     };
   }
 
@@ -115,10 +64,6 @@ export const getServerSideProps = getServerSidePropsWithUser(async ({ req, res, 
     redirect: {
       statusCode: 302,
       destination: '/login/success',
-    },
-
-    props: {
-      jwt,
     },
   };
 });
