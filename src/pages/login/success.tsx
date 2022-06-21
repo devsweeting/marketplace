4f5ed26310f2bee03b103ec782d926896debcc09
Frank Krogh <frank@buildmidwestern.com>
2022-06-21 11:12:19 -0500

    made route to api and changed back login

--- src/pages/login/success.tsx
+++ src/pages/login/success.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
 import { useLoginPageStyles } from '@/styles/LoginPage.styles';
 import { OpenGraph } from '@/components/OpenGraph';
 import { Box, Container, Typography } from '@mui/material';
@@ -6,9 +7,52 @@ import classNames from 'classnames';
 import { Button } from '@/components/Button';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import { getServerSidePropsWithUser } from '@/helpers/auth/withUser';
+import { getUserCookie } from '@/helpers/auth/userCookie';
 
-const Login: NextPage = () => {
+const Login: NextPage = (jwt) => {
   const classes = useLoginPageStyles();
+  const getWishList = async () => {
+    if (localStorage.getItem('wishList') && localStorage.getItem('wishList') !== 'undefined') {
+      const wishList = JSON.parse(localStorage.getItem('wishList') as string);
+      return wishList;
+    } else {
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    if (jwt) {
+      const fetchingWatchListToAPI = async (jwt: any) => {
+        const wishList = await getWishList();
+        if (!wishList || wishList === undefined || wishList.length <= 0) return;
+        for (let i = 0; i < wishList.length; i++) {
+          const item = wishList[i];
+          const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/watchlist`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${jwt.jwt}`,
+            },
+            body: JSON.stringify({
+              assetId: item.id,
+              userId: jwt.user.id,
+            }),
+          });
+
+          const data = await response.json();
+
+          return data;
+        }
+      };
+
+      fetchingWatchListToAPI(jwt).then((data) => {
+        if (data) {
+          localStorage.removeItem('wishList');
+        }
+      });
+    }
+  }, [jwt]);
+
   return (
     <>
       <OpenGraph title={'Login'} description={'Login page description'} />
@@ -40,7 +84,7 @@ const Login: NextPage = () => {
 
 export default Login;
 
-export const getServerSideProps = getServerSidePropsWithUser(async ({ user }) => {
+export const getServerSideProps = getServerSidePropsWithUser(async ({ req, user }) => {
   if (!user) {
     return {
       redirect: {
@@ -49,8 +93,11 @@ export const getServerSideProps = getServerSidePropsWithUser(async ({ user }) =>
       },
     };
   }
+  const jwt = getUserCookie(req);
 
   return {
-    props: {},
+    props: {
+      jwt,
+    },
   };
 });
