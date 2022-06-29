1ce730624620c839cd3a5157dda8cab62208bbec
nb-midwestern <nb@buildmidwestern.com>
2022-06-29 13:37:03 -0500

    add tests to user pane

--- src/__tests__/components/Navbar/components/UserPane.test.tsx
+++ src/__tests__/components/Navbar/components/UserPane.test.tsx
@@ -1,12 +1,13 @@
 import { UserPane } from '@/components/Navbar/components/UserPane';
+import { userPanelLinks } from '@/domain/userPaneLink';
 import { themeJump } from '@/styles/themeJump';
+import { withTestRouter } from '@/__tests__/utils/TestRouter';
 import { ThemeProvider } from '@mui/styles';
 import { render, screen } from '@testing-library/react';
 import user from '@testing-library/user-event';
-
-// import preview from 'jest-preview';
 import React from 'react';
 
+const mockLinks = userPanelLinks;
 jest.mock('next/router', () => ({
   useRouter() {
     return {
@@ -15,12 +16,14 @@ jest.mock('next/router', () => ({
     };
   },
 }));
+const push = jest.fn();
 
 const MockUserPane = () => {
-  return (
+  return withTestRouter(
     <ThemeProvider theme={themeJump}>
       <UserPane />
-    </ThemeProvider>
+    </ThemeProvider>,
+    { push, asPath: '/' },
   );
 };
 describe('UserPane', () => {
@@ -28,6 +31,26 @@ describe('UserPane', () => {
     render(<MockUserPane />);
     const userPanelBtn = screen.getByRole('button');
     await user.click(userPanelBtn);
-    // preview.debug();
+    expect(screen.getByText(/logout/i)).toBeInTheDocument;
+  });
+
+  test('should have all the links associated with user panel', async () => {
+    render(<MockUserPane />);
+    const userPanelBtn = screen.getByRole('button');
+    await user.click(userPanelBtn);
+    mockLinks.map((link) => {
+      expect(screen.getByRole('link', { name: link.title })).toHaveAttribute('href', link.path);
+    });
+  });
+
+  test('should route to specific link', async () => {
+    render(<MockUserPane />);
+    const userPanelBtn = screen.getByRole('button');
+    await user.click(userPanelBtn);
+    mockLinks.map(async (link) => {
+      const linkBtn = screen.getByRole('link', { name: link.title });
+      await user.click(linkBtn);
+      expect(push).toHaveBeenCalledWith(link.path, link.path, expect.anything());
+    });
   });
 });
