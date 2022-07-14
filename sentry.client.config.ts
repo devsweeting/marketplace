b34002a12cc304db583594128f07e6ab455879ba
nb-midwestern <nb@buildmidwestern.com>
2022-07-14 14:20:24 -0500

    use test dsn

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,5 +1,5 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: SENTRY_DSN || 'https://cb06fde608e64879a1df78bdf696bff9@o1318846.ingest.sentry.io/6573988',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
