1a87e7b6b4a978f630d4a9e1f26674f3673a3cef
Jared Harper <jared.kuolt@gmail.com>
2022-08-09 09:15:53 -0700

    Ignore 405s as well

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -23,8 +23,8 @@ MyError.getInitialProps = async (context) => {
   // getInitialProps has run
   errorInitialProps.hasGetInitialPropsRun = true;
 
-  // Returning early because we don't want to log 404 errors to Sentry.
-  if (res?.statusCode === 404) {
+  // Returning early because we don't want to log 404/405 errors to Sentry.
+  if (res?.statusCode === 404 || res?.statusCode === 405) {
     return errorInitialProps;
   }
 
