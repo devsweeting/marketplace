c88d5b3b7d7a42dc6ab74322ad10e2dcff2e5941
Frank Krogh <frank@buildmidwestern.com>
2022-09-13 09:18:22 -0500

    subscribe cta mobile styles

--- src/components/Homepage/SubscribeCallToAction/SubscribeCallToAction.styles.ts
+++ src/components/Homepage/SubscribeCallToAction/SubscribeCallToAction.styles.ts
@@ -1,7 +1,7 @@
 import { makeStyles } from '@mui/styles';
 
 export const useTextfieldStyles = makeStyles(
-  () => ({
+  (theme) => ({
     root: {
       '& .MuiFormLabel-root.Mui-error': {
         color: '#b04995',
@@ -11,17 +11,101 @@ export const useTextfieldStyles = makeStyles(
         fontSize: 20,
         fontWeight: 600,
         marginLeft: 20,
+        [theme.breakpoints.down('sm')]: {
+          fontSize: 18,
+        },
       },
       '& .MuiFormLabel-filled + .MuiInputBase-root input': {
         padding: '35px 12px 14px',
+        [theme.breakpoints.down('sm')]: {
+          padding: '15px 6px 7px',
+        },
       },
       '& .Mui-focused input': {
         padding: '35px 12px 14px',
+        [theme.breakpoints.down('sm')]: {
+          padding: '15px 6px 7px',
+        },
       },
       '& .MuiFormHelperText-root.Mui-error': {
         color: '#b04995',
         fontFamily: 'Muli',
         fontSize: 12,
+      },
+    },
+  }),
+  { name: 'textfield' },
+);
+
+export const useSubscribeCTAStyles = makeStyles(
+  (theme) => ({
+    ctaWrapper: {
+      width: '100%',
+      minWidth: '100%',
+      height: '300px',
+      backgroundColor: 'whitesmoke',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      position: 'relative',
+      overflow: 'hidden',
+      [theme.breakpoints.down('sm')]: {
+        height: '100vw',
+        padding: '0 10px',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      },
+    },
+    textfieldInput: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+      borderRadius: '50px',
+      width: '375px',
+      height: '75px',
+      marginLeft: '30px',
+      fontSize: '1.5rem',
+      border: '2px solid grey',
+      backgroundColor: 'white',
+      [theme.breakpoints.down('sm')]: {
+        margin: '10px 0px',
+        width: '95%',
+      },
+    },
+    submitForm: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      [theme.breakpoints.down('sm')]: {
+        width: '100%',
+        flexDirection: 'column',
+      },
+    },
+    ctaHeader: {
+      marginBottom: '15px',
+      fontSize: '3rem',
+      [theme.breakpoints.down('sm')]: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignText: 'center',
+        margin: '25px auto',
+        width: '100%',
+        fontSize: '2.4rem',
+      },
+    },
+    button: {
+      color: 'white',
+      backgroundColor: 'black',
+      borderRadius: '50px',
+      width: '175px',
+      height: '75px',
+      marginLeft: '30px',
+      fontSize: '1.3rem',
+      border: '3px solid black',
+      [theme.breakpoints.down('sm')]: {
+        margin: '0px',
       },
     },
   }),
--- src/components/Homepage/SubscribeCallToAction/SubscribeCallToAction.tsx
+++ src/components/Homepage/SubscribeCallToAction/SubscribeCallToAction.tsx
@@ -1,60 +1,24 @@
 import { Box, Button, TextField, Typography } from '@mui/material';
-import { useTextfieldStyles } from './SubscribeCallToAction.styles';
+import { useTextfieldStyles, useSubscribeCTAStyles } from './SubscribeCallToAction.styles';
 import React from 'react';
 
 export const SubscribeCallToAction = () => {
   const classes = useTextfieldStyles();
+  const styles = useSubscribeCTAStyles();
   return (
-    <Box
-      style={{
-        width: '100%',
-        minWidth: '100%',
-        height: '300px',
-        backgroundColor: 'whitesmoke',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        position: 'relative',
-        overflow: 'hidden',
-      }}
-    >
-      <Typography variant="h2" component="h2" style={{ marginBottom: '15px', fontSize: '3rem' }}>
+    <Box className={styles.ctaWrapper}>
+      <Typography variant="h2" component="h2" className={styles.ctaHeader}>
         Get weekly drops
       </Typography>
-      <form style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <form className={styles.submitForm}>
         <TextField
           InputProps={{ classes, disableUnderline: true }}
           id="outlined-basic"
           variant="standard"
           placeholder="Email"
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-            borderRadius: '50px',
-            width: '375px',
-            height: '75px',
-            marginLeft: '30px',
-            fontSize: '1.5rem',
-            border: '2px solid grey',
-            backgroundColor: 'white',
-          }}
+          className={styles.textfieldInput}
         />
-        <Button
-          style={{
-            color: 'white',
-            backgroundColor: 'black',
-            borderRadius: '50px',
-            width: '175px',
-            height: '75px',
-            marginLeft: '30px',
-            fontSize: '1.3rem',
-            border: '3px solid black',
-          }}
-        >
-          Subscribe
-        </Button>
+        <Button className={styles.button}>Subscribe</Button>
       </form>
     </Box>
   );
