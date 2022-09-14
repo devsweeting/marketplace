e77be9d21d329eebbdf8b32abdcfd81ad32e79f7
Frank Krogh <frank@buildmidwestern.com>
2022-09-14 15:59:05 -0500

    footer styles

--- src/layout/components/Footer/Footer.styles.ts
+++ src/layout/components/Footer/Footer.styles.ts
@@ -5,12 +5,62 @@ export const useFooterStyles = makeStyles(
     container: {
       textAlign: 'center',
       display: 'flex',
-      flexDirection: 'column',
-
       alignItems: 'center',
       height: '100%',
       width: '100%',
       backgroundColor: theme.palette.primary.main,
+    },
+    wrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      padding: '60px 20px 0 20px',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+    },
+    socialsAndResources: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '97%',
+      flexWrap: 'wrap',
+      [theme.breakpoints.down('sm')]: {
+        justifyContent: 'center',
+      },
+    },
+    socials: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      alignItems: 'flex-start',
+      height: '100%',
+      marginBottom: 70,
+      [theme.breakpoints.down('sm')]: {
+        alignItems: 'center',
+        width: '100%',
+      },
+    },
+    resources: {
+      display: 'flex',
+      alignItems: 'flex-start',
+      justifyContent: 'flex-end',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      width: '70%',
+      [theme.breakpoints.down('sm')]: {
+        justifyContent: 'space-between',
+        width: '100%',
+      },
+    },
+    logo: {
+      position: 'relative',
+      width: '113px',
+      height: '56px',
+      marginBottom: '10px',
+      [theme.breakpoints.down('sm')]: {
+        width: '150px',
+        height: '69px',
+      },
     },
     text: {
       fontFamily: 'Rubik',
--- src/layout/components/Footer/Footer.tsx
+++ src/layout/components/Footer/Footer.tsx
@@ -10,226 +10,236 @@ import Image from 'next/image';
 export const Footer = () => {
   const classes = useFooterStyles();
   return (
-    <Grid container item xs={12} flexDirection={'row'} justifyContent={'center'} id="footer">
-      <Grid item className={classes.container}>
-        <div
-          style={{
-            width: '97%',
-            maxWidth: '97%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <div
-            style={{
-              width: '97%',
-              height: '280px',
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'flex-start',
-              marginTop: '70px',
-            }}
-          >
+    <Grid container item xs={12} className={classes.container} id="footer">
+      <div className={classes.wrapper}>
+        <div className={classes.socialsAndResources}>
+          <Box className={classes.socials}>
+            <Box className={classes.logo}>
+              <Image
+                src={'/images/logoJump.svg'}
+                alt="Logo"
+                layout="fill"
+                style={{ filter: 'brightness(0) invert(1)' }}
+              />
+            </Box>
+            <Typography
+              variant="subtitle2"
+              component="p"
+              style={{ color: 'white', fontSize: 20, marginBottom: '15px' }}
+            >
+              {'NFT sport cards'}
+            </Typography>
+            <ul
+              style={{
+                display: 'flex',
+                listStyle: 'none',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                margin: 0,
+                padding: 0,
+                width: '100%',
+              }}
+            >
+              <li>
+                <FacebookIcon style={{ color: 'white', fontSize: 24 }} />
+              </li>
+              <li>
+                <InstagramIcon style={{ color: 'white', fontSize: 24 }} />
+              </li>
+              <li>
+                <TwitterIcon style={{ color: 'white', fontSize: 24 }} />
+              </li>
+              <li>
+                <GitHubIcon style={{ color: 'white', fontSize: 24 }} />
+              </li>
+            </ul>
+          </Box>
+          <div className={classes.resources}>
             <Box
               style={{
                 display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-around',
                 alignItems: 'flex-start',
-                height: '170px',
-                marginBottom: 70,
+                justifyContent: 'flex-end',
+                flexDirection: 'column',
+                width: '33.33%',
               }}
             >
-              <Box style={{ position: 'relative', width: '113px', height: '56px' }}>
-                <Image
-                  src={'/images/logoJump.svg'}
-                  alt="Logo"
-                  layout="fill"
-                  style={{ filter: 'brightness(0) invert(1)' }}
-                />
-              </Box>
               <Typography
                 variant="subtitle2"
                 component="p"
                 style={{ color: 'white', fontSize: 20 }}
               >
-                {'NFT sport cards'}
+                {'Jump'}
               </Typography>
               <ul
                 style={{
                   display: 'flex',
                   listStyle: 'none',
-                  alignItems: 'center',
-                  justifyContent: 'space-between',
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-start',
+                  flexDirection: 'column',
                   margin: 0,
                   padding: 0,
-                  width: '250px',
+                  width: '100%',
                 }}
               >
-                <li>
-                  <FacebookIcon style={{ color: 'white', fontSize: 24 }} />
-                </li>
-                <li>
-                  <InstagramIcon style={{ color: 'white', fontSize: 24 }} />
-                </li>
-                <li>
-                  <TwitterIcon style={{ color: 'white', fontSize: 24 }} />
-                </li>
-                <li>
-                  <GitHubIcon style={{ color: 'white', fontSize: 24 }} />
-                </li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>About us</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Careers</li>
               </ul>
             </Box>
-            <div style={{ display: 'flex', width: '60%', justifyContent: 'space-between' }}>
-              <Box
+            <Box
+              style={{
+                display: 'flex',
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                flexDirection: 'column',
+                width: '33.33%',
+              }}
+            >
+              <Typography
+                variant="subtitle2"
+                component="p"
+                style={{ color: 'white', fontSize: 20 }}
+              >
+                {'Product'}
+              </Typography>
+              <ul
                 style={{
                   display: 'flex',
+                  listStyle: 'none',
                   alignItems: 'flex-start',
                   justifyContent: 'flex-start',
                   flexDirection: 'column',
+                  margin: 0,
+                  padding: 0,
+                  width: '100%',
                 }}
               >
-                <Typography
-                  variant="subtitle2"
-                  component="p"
-                  style={{ color: 'white', fontSize: 20 }}
-                >
-                  {'Jump'}
-                </Typography>
-                <ul
-                  style={{
-                    display: 'flex',
-                    listStyle: 'none',
-                    alignItems: 'flex-start',
-                    justifyContent: 'flex-start',
-                    flexDirection: 'column',
-                    margin: 0,
-                    padding: 0,
-                    width: '250px',
-                  }}
-                >
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>About us</li>
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Careers</li>
-                </ul>
-              </Box>
-              <Box
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Marketplace</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Search</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>PWCC</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Fractions</li>
+              </ul>
+            </Box>
+            <Box
+              style={{
+                display: 'flex',
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                flexDirection: 'column',
+              }}
+            >
+              <Typography
+                variant="subtitle2"
+                component="p"
+                style={{ color: 'white', fontSize: 20 }}
+              >
+                {'Resources'}
+              </Typography>
+              <ul
                 style={{
                   display: 'flex',
+                  listStyle: 'none',
                   alignItems: 'flex-start',
                   justifyContent: 'flex-start',
                   flexDirection: 'column',
+                  margin: 0,
+                  padding: 0,
+                  width: '100%',
                 }}
               >
-                <Typography
-                  variant="subtitle2"
-                  component="p"
-                  style={{ color: 'white', fontSize: 20 }}
-                >
-                  {'Product'}
-                </Typography>
-                <ul
-                  style={{
-                    display: 'flex',
-                    listStyle: 'none',
-                    alignItems: 'flex-start',
-                    justifyContent: 'flex-start',
-                    flexDirection: 'column',
-                    margin: 0,
-                    padding: 0,
-                    width: '250px',
-                  }}
-                >
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Marketplace</li>
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Search</li>
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>PWCC</li>
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Fractions</li>
-                </ul>
-              </Box>
-              <Box
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>FAQ</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Help</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Blog</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>
+                  Join our community
+                </li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Newsletter</li>
+              </ul>
+            </Box>
+            <Box
+              style={{
+                display: 'flex',
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                flexDirection: 'column',
+              }}
+            >
+              <Typography
+                variant="subtitle2"
+                component="p"
+                style={{ color: 'white', fontSize: 20 }}
+              >
+                {'Resources'}
+              </Typography>
+              <ul
                 style={{
                   display: 'flex',
+                  listStyle: 'none',
                   alignItems: 'flex-start',
                   justifyContent: 'flex-start',
                   flexDirection: 'column',
+                  margin: 0,
+                  padding: 0,
+                  width: '100%',
                 }}
               >
-                <Typography
-                  variant="subtitle2"
-                  component="p"
-                  style={{ color: 'white', fontSize: 20 }}
-                >
-                  {'Resources'}
-                </Typography>
-                <ul
-                  style={{
-                    display: 'flex',
-                    listStyle: 'none',
-                    alignItems: 'flex-start',
-                    justifyContent: 'flex-start',
-                    flexDirection: 'column',
-                    margin: 0,
-                    padding: 0,
-                    width: '250px',
-                  }}
-                >
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>FAQ</li>
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Help</li>
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Blog</li>
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>
-                    Join our community
-                  </li>
-                  <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Newsletter</li>
-                </ul>
-              </Box>
-            </div>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>FAQ</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Help</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Blog</li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>
+                  Join our community
+                </li>
+                <li style={{ color: 'white', fontSize: 16, margin: '10px 0' }}>Newsletter</li>
+              </ul>
+            </Box>
           </div>
-          <Divider
-            sx={{
-              backgroundColor: 'white',
-              width: '97%',
-              height: '2px',
-              margin: '10px 0',
-              opacity: '0.3',
-              borderRadius: '2px',
-            }}
-          />
-          <div
+        </div>
+        <Divider
+          sx={{
+            backgroundColor: 'white',
+            width: '97%',
+            height: '2px',
+            margin: '10px 0',
+            opacity: '0.3',
+            borderRadius: '2px',
+          }}
+        />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            width: '100%',
+          }}
+        >
+          <Box
             style={{
               display: 'flex',
-              justifyContent: 'center',
-              width: '100%',
+              justifyContent: 'space-between',
+              width: '97%',
             }}
           >
+            <Typography variant="body1" component="p" className={classes.text}>
+              © 2022 Jump. All rights reserved.
+            </Typography>
             <Box
               style={{
                 display: 'flex',
                 justifyContent: 'space-between',
-                width: '97%',
+                alignItems: 'center',
+                width: '20%',
               }}
             >
               <Typography variant="body1" component="p" className={classes.text}>
-                © 2022 Jump. All rights reserved.
+                {'Privacy Policy'}
               </Typography>
-              <Box
-                style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  width: '20%',
-                }}
-              >
-                <Typography variant="body1" component="p" className={classes.text}>
-                  {'Privacy Policy'}
-                </Typography>
-                <Typography variant="body1" component="p" className={classes.text}>
-                  {'Terms & Conditions'}
-                </Typography>
-              </Box>
+              <Typography variant="body1" component="p" className={classes.text}>
+                {'Terms & Conditions'}
+              </Typography>
             </Box>
-          </div>
+          </Box>
         </div>
-      </Grid>
+      </div>
     </Grid>
   );
 };
