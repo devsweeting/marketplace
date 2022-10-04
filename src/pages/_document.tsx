9f5838527136f12d7ad639d8ca2c0430cf8b4544
lzustiak <lz@buildmidwestern.com>
2022-10-04 16:57:02 -0500

    [feat] add Inter font to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;800&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
 
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {(this.props as any).emotionStyleTags}
