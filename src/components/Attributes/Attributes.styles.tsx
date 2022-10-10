de11cdf63a3442d4d68ef944906d90bbff4aaa54
lzustiak <lz@buildmidwestern.com>
2022-10-10 15:32:17 -0500

    [feat] add attribute component

---
+++ src/components/Attributes/Attributes.styles.tsx
@@ -0,0 +1,10 @@
+import { styled, Typography } from '@mui/material';
+
+export const Container = styled('div')({
+  display: 'flex',
+  gap: '1rem',
+});
+
+export const Attribute = styled(Typography)({
+  fontWeight: 500,
+});
