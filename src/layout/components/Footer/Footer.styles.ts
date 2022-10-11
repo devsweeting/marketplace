686298834dbc5ca20671e467ee18070cd35f69ee
lzustiak <lz@buildmidwestern.com>
2022-10-11 13:37:51 -0500

    [feat] updates to Footer component

--- src/layout/components/Footer/Footer.styles.ts
+++ src/layout/components/Footer/Footer.styles.ts
@@ -1,104 +1,41 @@
-import { makeStyles } from '@mui/styles';
+import { styled } from '@mui/material';
 
-export const useFooterStyles = makeStyles(
-  (theme) => ({
-    container: {
-      textAlign: 'center',
-      display: 'flex',
-      alignItems: 'center',
-      height: '100%',
-      width: '100%',
-      backgroundColor: theme.palette.primary.main,
-    },
-    wrapper: {
-      display: 'flex',
-      flexDirection: 'column',
-      padding: '60px 20px 0 20px',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%',
-    },
-    socialsAndResources: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      width: '97%',
-      flexWrap: 'wrap',
-      [theme.breakpoints.down('sm')]: {
-        justifyContent: 'center',
-      },
-    },
-    socials: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      alignItems: 'flex-start',
-      height: '100%',
-      marginBottom: 70,
-      [theme.breakpoints.down('sm')]: {
-        alignItems: 'center',
-        width: '100%',
-      },
-    },
-    resources: {
-      display: 'flex',
-      alignItems: 'flex-start',
-      justifyContent: 'flex-end',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      width: '70%',
-      [theme.breakpoints.down('sm')]: {
-        justifyContent: 'space-between',
-        width: '100%',
-      },
-    },
-    resourceText: {
-      color: 'white',
-      fontSize: 16,
-      margin: '10px 0',
-      padding: 0,
-      [theme.breakpoints.down('sm')]: {
-        fontSize: 14,
-        margin: '15px 0',
-      },
-    },
-    logo: {
-      position: 'relative',
-      width: '113px',
-      height: '56px',
-      marginBottom: '10px',
-      [theme.breakpoints.down('sm')]: {
-        width: '150px',
-        height: '69px',
-      },
-    },
-    footer: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      width: '97%',
-      [theme.breakpoints.down('sm')]: {
-        flexWrap: 'wrap-reverse',
-        justifyContent: 'center',
-      },
-    },
-    termsAndPolicy: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '20%',
-      [theme.breakpoints.down('sm')]: {
-        width: '100%',
-      },
-    },
-    text: {
-      fontFamily: 'Rubik',
-      color: 'white',
-      fontWeight: 300,
-      fontSize: '16px',
-      lineHeight: '17px',
-      opacity: '0.8',
-      margin: '24px 0px',
-    },
-  }),
-  { name: 'footer' },
-);
+export const Container = styled('div')(({ theme }) => ({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  color: theme.palette.secondary.main,
+  backgroundColor: theme.palette.primary.main,
+  padding: '2rem',
+  gap: '2rem',
+}));
+
+export const TopContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '1rem',
+  margin: '1rem 0',
+});
+
+export const Img = styled('img')({});
+
+export const SocialLinks = styled('div')({
+  display: 'flex',
+  gap: '2rem',
+});
+
+export const BottomContainer = styled('div')({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  flexWrap: 'wrap',
+  gap: '1rem',
+});
+
+export const PageLinks = styled('div')({
+  display: 'flex',
+  flexWrap: 'wrap',
+  gap: '1rem 1.5rem',
+});
