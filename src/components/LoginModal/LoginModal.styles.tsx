3ec6735def4acc56505d2c251719645539ecbd68
Frank Krogh <frank@buildmidwestern.com>
2022-10-13 16:24:27 -0500

    updated login modal

---
+++ src/components/LoginModal/LoginModal.styles.tsx
@@ -0,0 +1,114 @@
+import { styled, Modal as MuiModal, Card, lighten, TextField } from '@mui/material';
+
+export const Modal = styled(MuiModal)({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+export const Container = styled('div')(({ theme }) => ({
+  margin: '15px 15px 15px 0',
+  height: '43px',
+  width: '80%',
+  [theme.breakpoints.down('md')]: {
+    width: '100%',
+  },
+}));
+
+export const Message = styled('span')({
+  display: 'block',
+  height: 12,
+  fontSize: '14px',
+  color: '#666',
+});
+
+export const Form = styled('form')({
+  marginTop: '1rem',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+});
+
+export const Label = styled('label')({
+  position: 'absolute',
+  left: '-10000px',
+  top: 'auto',
+  width: '1px',
+  height: '1px',
+  overflow: 'hidden',
+});
+export const title = {
+  color: 'black',
+};
+
+export const ModalCard = styled(Card)(({ theme }) => ({
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  padding: '8px 32px',
+  position: 'absolute' as const,
+  top: '45%',
+  left: '50%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '800px',
+  height: '470px',
+  backgroundColor: 'white',
+  borderRadius: '10px',
+  boxShadow: `0px 2px 4px -1px ${lighten(
+    theme.palette.primary.main,
+    0.2,
+  )}, 0px 4px 5px 0px ${lighten(theme.palette.primary.main, 0.2)}, 0px 1px 10px 0px ${lighten(
+    theme.palette.primary.main,
+    0.2,
+  )}`,
+  outline: 'none !important',
+  [theme.breakpoints.down('md')]: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '500px',
+    width: '100%',
+    height: '470px',
+  },
+}));
+
+export const InputTextField = styled(TextField)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  width: '100%',
+  height: '68px',
+  fontSize: '1.5rem',
+  borderRadius: '8px',
+  backgroundColor: '#F3F4F6',
+  '& label': {
+    margin: '12px 0 0 16px',
+    fontSize: '16px',
+
+    color: theme.palette.primary.main,
+    '&.Mui-focused': {
+      marginLeft: '16px',
+    },
+  },
+  '& div.MuiInputBase-root.MuiInput-root': {
+    marginTop: '0',
+    width: '100%',
+  },
+  '& input': {
+    color: theme.palette.primary.main,
+    fontSize: 20,
+    fontWeight: 600,
+    margin: '12px 0 0 16px',
+    backgroundColor: '#F3F4F6',
+    padding: '0',
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: 18,
+    },
+  },
+  [theme.breakpoints.down('sm')]: {
+    margin: '10px 0px',
+    width: '95%',
+  },
+}));
--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,154 +1,409 @@
-import { useEffect, useRef, useState } from 'react';
-import { Box, OutlinedInput, Typography, useTheme } from '@mui/material';
-import { Button } from '@/components/Button';
+import { useEffect, useReducer, useRef, useState } from 'react';
+import { Typography, useTheme, Button, alpha } from '@mui/material';
+import { Box } from '@mui/system';
+import type { Theme } from '@mui/material';
 import { useModal } from '@/helpers/hooks/useModal';
 import { loginRequest } from '@/api/endpoints/loginRequest';
 import { StatusCodes } from 'http-status-codes';
 import { useRouter } from 'next/router';
+import { apiClient } from '@/api/client';
 
-import { Modal, ModalCard, Form, Label } from './LoginModal.styles';
-import Link from 'next/link';
+import { Modal, ModalCard, InputTextField } from './LoginModal.styles';
+
+interface ILoginState {
+  isLoading: boolean;
+  statCode: number;
+  error: string;
+}
+
+type LoginAction =
+  | { type: 'fetching' }
+  | { type: 'success'; payload: StatusCodes }
+  | { type: 'fail'; payload: StatusCodes }
+  | { type: 'error'; error: Error };
+
+const initialLoginState: ILoginState = {
+  isLoading: false,
+  statCode: 0,
+  error: '',
+};
+
+export const validate = (email: string) => {
+  if (email.length === 0) {
+    return false;
+  }
+  if (!email.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)) {
+    return false;
+  }
+  return true;
+};
+
+const LoginInput = ({
+  theme,
+  statCode,
+  loginInputValue,
+  setLoginInputValue,
+  tokenInputValue,
+  setTokenInputValue,
+  handleLoginSubmit,
+  handleTokenSubmit,
+}: {
+  theme: Theme;
+  statCode: number;
+  loginInputValue: any;
+  setLoginInputValue: any;
+  tokenInputValue: any;
+  setTokenInputValue: any;
+  handleLoginSubmit: any;
+  handleTokenSubmit: any;
+}) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '170px',
+      }}
+    >
+      {statCode === 0 && (
+        <div role="form">
+          <Typography
+            sx={{
+              textTransform: 'uppercase',
+              fontWeight: '600',
+              fontSize: '16px',
+              lineHeight: '100%',
+            }}
+          >
+            give us your email we&#39;ll send you a magic link to log in.
+          </Typography>
+          <InputTextField
+            type="email"
+            label={'Email'}
+            name={'email'}
+            InputLabelProps={{ shrink: true }}
+            InputProps={{ disableUnderline: true, name: 'email' }}
+            id="email"
+            variant="standard"
+            placeholder="Email"
+            value={loginInputValue}
+            onChange={(newValue: { target: { value: string } }) =>
+              setLoginInputValue(newValue.target.value)
+            }
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                setLoginInputValue((e.target as unknown as HTMLTextAreaElement).value);
+                handleLoginSubmit(loginInputValue);
+              }
+            }}
+          />
+          <Typography>You can use your PWCC email, we will send you a token.</Typography>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              marginRight: '0px',
+              flexDirection: 'column',
+              alignItems: 'flex-end',
+              justifyContent: 'center',
+            }}
+          >
+            <Button
+              id={'loginButton'}
+              role="button"
+              sx={{
+                '&.MuiButtonBase-root': {
+                  color: 'white',
+                  backgroundColor: 'black',
+                  width: '100%',
+                  height: '56px',
+                  margin: '20px 0px',
+                  fontSize: '1.3rem',
+                  border: '3px solid black',
+                  '&:hover': {
+                    color: 'black',
+                  },
+                  [theme.breakpoints.down('sm')]: {
+                    margin: '10px auto',
+                  },
+                },
+              }}
+              onClick={() => {
+                handleLoginSubmit(loginInputValue);
+              }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </div>
+      )}
+      {statCode === StatusCodes.OK && (
+        <form role="form">
+          <InputTextField
+            label={'code'}
+            name={'code'}
+            InputLabelProps={{ shrink: true }}
+            InputProps={{ disableUnderline: true }}
+            id="outlined-basic"
+            variant="standard"
+            placeholder="Code"
+            value={tokenInputValue}
+            onChange={(newValue: { target: { value: string } }) =>
+              setTokenInputValue(newValue.target.value)
+            }
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                setTokenInputValue((e.target as unknown as HTMLTextAreaElement).value);
+                void handleTokenSubmit(tokenInputValue);
+              }
+            }}
+          />
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              marginRight: '0px',
+              flexDirection: 'column',
+              alignItems: 'flex-end',
+              justifyContent: 'center',
+            }}
+          >
+            <Button
+              id={'loginButton'}
+              sx={{
+                '&.MuiButtonBase-root': {
+                  color: 'white',
+                  backgroundColor: 'black',
+                  borderRadius: '50px',
+                  width: '175px',
+                  height: '56px',
+                  margin: '20px 10px',
+                  fontSize: '1.3rem',
+                  border: '3px solid black',
+                  '&:hover': {
+                    color: 'black',
+                  },
+                  [theme.breakpoints.down('sm')]: {
+                    margin: '10px auto',
+                  },
+                },
+              }}
+              onClick={() => {
+                void handleTokenSubmit(tokenInputValue);
+              }}
+            >
+              Submit
+            </Button>
+          </Box>
+        </form>
+      )}
+    </Box>
+  );
+};
+
+const loginReducer = (state: ILoginState, action: LoginAction) => {
+  switch (action.type) {
+    case 'fetching': {
+      return { ...state, isLoading: true, error: '' };
+    }
+    case 'success': {
+      return { ...state, isLoading: false, statCode: action.payload };
+    }
+    case 'fail': {
+      return { ...state, isLoading: false, statCode: action.payload };
+    }
+    case 'error': {
+      return { ...state, isLoading: false, error: action.error.message };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
 export const LoginModal = ({ open: isOpen, noDismiss }: { open: boolean; noDismiss?: boolean }) => {
-  const [emailState, setEmailState] = useState('');
-  const [alertMessage, setAlertMessage] = useState('');
+  const theme = useTheme();
+  const [loginInputValue, setLoginInputValue] = useState('');
+  const [tokenInputValue, setTokenInputValue] = useState('');
+  const [headerText, setHeaderText] = useState('Login/Signup');
+  const [alertMessage, setAlertMessage] = useState(' ');
+
   const [buttonState, setButtonState] = useState(false);
   const { setIsModalOpen } = useModal();
   const modalBox = useRef<HTMLDivElement>(null);
   const router = useRouter();
-  const theme = useTheme();
+  const [{ isLoading, error, statCode }, dispatch] = useReducer(loginReducer, initialLoginState);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmailState(event.target.value);
-    setButtonState(false);
-  };
-  const handleClose = () => {
-    if (noDismiss) {
-      setIsModalOpen(!isOpen);
+  const handleClose = (event: any, reason: string) => {
+    if (reason && reason == 'backdropClick' && noDismiss) {
+      return;
     }
-
-    setEmailState('');
+    setIsModalOpen(!isOpen);
+    setLoginInputValue('');
+    if (statCode === 0 || statCode === StatusCodes.TOO_MANY_REQUESTS) {
+      setHeaderText('Login/Signup');
+      setAlertMessage('');
+    }
     setButtonState(false);
-    setAlertMessage('');
   };
 
   useEffect(() => {
     //
-  }, [emailState, buttonState, alertMessage]);
+  }, [loginInputValue, tokenInputValue, buttonState, alertMessage]);
 
-  const validate = (email: string) => {
-    if (email.length === 0) {
-      return false;
-    }
-    if (!email.match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)) {
-      return false;
-    }
-    return true;
-  };
-
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-
-    validate(emailState);
-    if (!validate(emailState)) {
-      setAlertMessage('Please enter a valid email address');
-      if (modalBox.current) {
-        modalBox.current.style.color = '#f44336';
-      }
+  const handleLoginSubmit = (value: string) => {
+    if (!validate(value)) {
+      setHeaderText('Enter a valid email');
+      setAlertMessage('Example: john.smith@example.com');
       return;
     }
-
-    loginRequest(emailState)
+    dispatch({ type: 'fetching' });
+    loginRequest(value)
       .then((status) => {
         switch (status) {
           case StatusCodes.OK:
-            setAlertMessage('Check your email for a link to sign in.');
-            if (modalBox.current) {
-              modalBox.current.style.color = '#4caf50';
-            }
-            setButtonState(true);
+            dispatch({ type: 'success', payload: status });
+            setHeaderText('Check your email');
+            setAlertMessage(`We emailed a magic link to ${loginInputValue}.
+            Please click the link to login or signup.
+            Or enter code below
+            `);
+
             return;
           case StatusCodes.TOO_MANY_REQUESTS:
-            setAlertMessage('Too many requests. Please try again later.');
-            if (modalBox.current) {
-              modalBox.current.style.color = '#f44336';
-            }
-            setButtonState(true);
+            dispatch({ type: 'fail', payload: status });
+            setAlertMessage('Too many requests');
             return;
           default:
-            if (modalBox.current) {
-              setAlertMessage('Something went wrong. Please try again later.');
-              modalBox.current.style.color = '#ffae00';
-            }
+            dispatch({ type: 'fail', payload: 408 });
+            return;
         }
       })
-      .catch(() => {
-        if (modalBox.current) {
-          setAlertMessage('Something went wrong. Please try again later.');
-          modalBox.current.style.color = '#ffae00';
-        }
-      })
-      .finally(() => {
-        setButtonState(true);
+      .catch((error) => {
+        dispatch({ type: 'error', error });
       });
+  };
+
+  const handleTokenSubmit = async (token: string) => {
+    const response = await apiClient.post('/login/confirm', {
+      requireAuth: false,
+      body: {
+        token: token,
+      },
+    });
+    if (!response.ok) {
+      return;
+    }
+    router.reload();
   };
   return (
     <Modal
+      sx={{
+        outline: 'none',
+        '.MuiBackdrop-root': {
+          backgroundColor: (theme) => alpha(theme.palette.primary.main, 0.85),
+        },
+      }}
       open={isOpen}
       onClose={handleClose}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
       <ModalCard>
-        <Typography id="modal-modal-title" variant="xl4" fontWeight={700}>
-          Login
-        </Typography>
+        {statCode === 0 && headerText === 'Login/Signup' ? (
+          <>
+            <Box sx={{ marginBottom: '40px', width: '100%' }}>
+              <Typography
+                variant="xl2"
+                component="h2"
+                sx={{
+                  fontSize: '72px',
+                  lineHeight: '72px',
+                  [theme.breakpoints.down('md')]: {
+                    fontSize: '56px',
+                  },
+                }}
+              >
+                {headerText}
+              </Typography>
+              <span style={{ marginBottom: '40px' }} role="alert" ref={modalBox}>
+                {alertMessage}
+              </span>
+              <Typography
+                variant="lg"
+                sx={{
+                  marginTop: '60px',
+                  fontSize: '24px',
+                  lineHeight: '32px',
+                  fontWeight: '500',
+                  [theme.breakpoints.down('md')]: {
+                    marginTop: '40px',
+                  },
+                }}
+              >
+                Save your favourite cards and get notified…
+              </Typography>
+            </Box>
 
-        <Typography variant="body2" color="error" role="alert" ref={modalBox}>
-          {alertMessage}
-        </Typography>
-        <Form onSubmit={handleSubmit} data-testid="form">
-          <Label htmlFor="email" aria-label="email">
-            email
-          </Label>
-          <OutlinedInput
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Email"
-            onChange={handleChange}
-            value={emailState}
-          />
-          <Button id="submit" variant="contained" disabled={buttonState} name="Login" type="submit">
-            Login
-          </Button>
-        </Form>
-        {noDismiss ? (
-          <Box display="flex" justifyContent="space-between" marginTop="20px">
-            <Link href={''}>
-              <a
-                onClick={() => {
-                  router.back();
-                }}
-                style={{
-                  color: theme.palette.primary.main,
-                }}
-              >
-                Go back
-              </a>
-            </Link>
-            <Link href={'/explore'}>
-              <a
-                style={{
-                  color: theme.palette.primary.main,
-                }}
-              >
-                Explore
-              </a>
-            </Link>
-          </Box>
+            <Box
+              sx={{
+                width: '100%',
+              }}
+            >
+              {!isLoading && !error && (
+                <LoginInput
+                  theme={theme}
+                  statCode={statCode}
+                  loginInputValue={loginInputValue}
+                  setLoginInputValue={setLoginInputValue}
+                  tokenInputValue={tokenInputValue}
+                  setTokenInputValue={setTokenInputValue}
+                  handleLoginSubmit={handleLoginSubmit}
+                  handleTokenSubmit={handleTokenSubmit}
+                />
+              )}
+            </Box>
+          </>
         ) : (
-          ''
+          <>
+            <Box sx={{ marginBottom: '40px', width: '100%' }}>
+              <Typography
+                variant="xl2"
+                component="h2"
+                sx={{
+                  fontSize: '72px',
+                  lineHeight: '72px',
+                  [theme.breakpoints.down('md')]: {
+                    fontSize: '56px',
+                  },
+                }}
+              >
+                {headerText}
+              </Typography>
+              <Typography style={{ marginBottom: '40px' }} role="alert" ref={modalBox}>
+                {alertMessage}
+              </Typography>
+            </Box>
+
+            <Box
+              sx={{
+                width: '100%',
+              }}
+            >
+              {!isLoading && !error && (
+                <LoginInput
+                  theme={theme}
+                  statCode={statCode}
+                  loginInputValue={loginInputValue}
+                  setLoginInputValue={setLoginInputValue}
+                  tokenInputValue={tokenInputValue}
+                  setTokenInputValue={setTokenInputValue}
+                  handleLoginSubmit={handleLoginSubmit}
+                  handleTokenSubmit={handleTokenSubmit}
+                />
+              )}
+            </Box>
+          </>
         )}
       </ModalCard>
     </Modal>
