1c1a6d416f2a6e3dc396c8162e3004c969a12939
lzustiak <lz@buildmidwestern.com>
2022-10-19 16:04:44 -0500

    [refactor] remove unused faq components and tests

--- src/components/Accordion/Accordion.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-export const Accordion = ({ children }: { children: JSX.Element }) => {
-  return <div>{children}</div>;
-};
--- src/components/FaqSearchContainer/components/SearchBox/Searchbox.styles.ts
+++
@@ -1,5 +0,0 @@
-import { OutlinedInput as MuiOutlinedInput, styled } from '@mui/material';
-
-export const OutlinedInput = styled(MuiOutlinedInput)(({ theme }) => ({
-  backgroundColor: theme.palette.secondary.main,
-}));
