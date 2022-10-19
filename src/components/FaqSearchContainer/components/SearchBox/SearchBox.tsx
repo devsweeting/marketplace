5c4a0e7128c1e08ba4bb7ae9f03d45a79993a908
lzustiak <lz@buildmidwestern.com>
2022-10-19 01:25:47 -0500

    [refactor] update Searchbox styles

--- src/components/FaqSearchContainer/components/SearchBox/SearchBox.tsx
+++ src/components/FaqSearchContainer/components/SearchBox/SearchBox.tsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import OutlinedInput from '@mui/material/OutlinedInput';
 import InputAdornment from '@mui/material/InputAdornment';
 import { IconButton } from '@mui/material';
-import FormControl from '@mui/material/FormControl';
-import { useSearchboxStyles } from './Searchbox.styles';
+import { OutlinedInput } from './Searchbox.styles';
 
 export const SearchBox = () => {
-  const classes = useSearchboxStyles();
   const [value, setValue] = useState('');
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,22 +12,21 @@ export const SearchBox = () => {
   };
 
   return (
-    <FormControl variant="outlined" className={classes.wrapper}>
-      <OutlinedInput
-        role={'textbox'}
-        aria-label={'search box'}
-        name="q"
-        value={value}
-        onChange={handleChange}
-        placeholder={'Search by topic, keyword etc.'}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton type="submit" role={'button'}>
-              <SearchIcon aria-label="search icon" />
-            </IconButton>
-          </InputAdornment>
-        }
-      />
-    </FormControl>
+    <OutlinedInput
+      role={'textbox'}
+      aria-label={'search box'}
+      name="q"
+      value={value}
+      onChange={handleChange}
+      placeholder={'Search by topic, keyword etc.'}
+      fullWidth
+      endAdornment={
+        <InputAdornment position="end">
+          <IconButton type="submit" role={'button'}>
+            <SearchIcon aria-label="search icon" />
+          </IconButton>
+        </InputAdornment>
+      }
+    />
   );
 };
--- src/components/FaqSearchContainer/components/SearchBox/Searchbox.styles.ts
+++ src/components/FaqSearchContainer/components/SearchBox/Searchbox.styles.ts
@@ -1,29 +1,5 @@
-import { makeStyles } from '@mui/styles';
+import { OutlinedInput as MuiOutlinedInput, styled } from '@mui/material';
 
-export const useSearchboxStyles = makeStyles((theme) => ({
-  wrapper: {
-    width: '100%',
-    '& .MuiInputBase-root': {
-      paddingLeft: '10px',
-      borderRadius: '56px',
-      height: '56px',
-      fontFamily: 'Montserrat',
-      color: theme.palette.primary,
-      fontSize: 'calc(10px + 0.4vw) !important',
-      fontWeight: '400 !important',
-      lineHeight: '150 !important',
-      letterSpacing: '0.15px !important',
-      textTransform: 'none',
-      background: theme.palette.secondary.main,
-      '& input:-webkit-autofill::first-line': {
-        fontSize: '34px !important',
-      },
-      '& input:-webkit-autofill::first-line, input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, textarea:-webkit-autofill, textarea:-webkit-autofill:hover, textarea:-webkit-autofill:focus, select:-webkit-autofill, select:-webkit-autofill:hover, select:-webkit-autofill:focus':
-        {
-          transition: 'background-color 5000s',
-          '-webkit-text-fill-color': '#fff !important',
-          caretColor: '#fff !important',
-        },
-    },
-  },
+export const OutlinedInput = styled(MuiOutlinedInput)(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.main,
 }));
