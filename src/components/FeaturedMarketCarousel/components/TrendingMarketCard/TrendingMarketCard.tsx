b45a1d9315529f21d6e6f0151177cfd2725d5e0c
lzustiak <lz@buildmidwestern.com>
2022-10-19 01:27:48 -0500

    [refactor] update TrendingMarketCard styles

--- src/components/FeaturedMarketCarousel/components/TrendingMarketCard/TrendingMarketCard.tsx
+++ src/components/FeaturedMarketCarousel/components/TrendingMarketCard/TrendingMarketCard.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Typography, Card } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Image from 'next/image';
-import { useTrendingMarketCardStyles } from './TrendingMarketCard.styles';
 import type { IMarket } from '@/types/assetTypes';
 import { formatNumber } from '@/helpers/formatNumber';
 import { useRouter } from 'next/router';
+import { Card, ImgWrapper, TextContainer } from './TrendingMarketCard.styles';
 
 export const TrendingMarketCard = ({
   brand,
@@ -18,7 +18,6 @@ export const TrendingMarketCard = ({
   activeCard?: string;
 }) => {
   const [activated, setActivated] = useState<string | undefined>('');
-  const classes = useTrendingMarketCardStyles();
   const router = useRouter();
   const { query, isReady } = router;
 
@@ -35,48 +34,35 @@ export const TrendingMarketCard = ({
   if (!onClick) return null;
   return (
     <Box
-      className={classes.marketCardContainer}
-      id={'marketCard'}
       tabIndex={tabIndex}
       onClick={() => {
         onClick();
       }}
+      sx={{ scrollSnapAlign: 'start' }}
     >
-      <Card
-        variant="outlined"
-        className={`${classes.card} ${activated === brand.brand ? classes.active : ''}`}
-      >
-        <Box className={classes.assetImageInnerContainer}>
+      <Card variant="outlined" active={activated === brand.brand}>
+        <ImgWrapper>
           {/* TODO: Update this when data is available */}
           <Image
             src={'/images/No_image_available_500_x_500.svg'}
             alt={'No Image Available'}
             layout="fill"
           />
-        </Box>
+        </ImgWrapper>
 
-        <Box className={classes.assetTextContainer}>
+        <TextContainer>
           {brand.brand && (
-            <Typography variant="xl5" component="h4" className={classes.cardTitle} id="cardTitle">
+            <Typography variant="body1" fontWeight={700}>
               {brand.brand}
             </Typography>
           )}
 
-          <div
-            style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              justifyContent: 'space-between',
-              marginTop: '20px',
-            }}
-          >
-            {brand.value_dollars && (
-              <Typography variant="xl5" component="h4" className={classes.cardSubTitle}>
-                Valuation: {'$' + formatNumber(brand.value_dollars)}
-              </Typography>
-            )}
-          </div>
-        </Box>
+          {brand.value_dollars && (
+            <Typography variant="body2" fontWeight={700}>
+              Valuation: {'$' + formatNumber(brand.value_dollars)}
+            </Typography>
+          )}
+        </TextContainer>
       </Card>
     </Box>
   );
