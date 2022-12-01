2dbb64dca4bb927c7a6c7e9491300f6d57af3508
Frank Krogh <frank@buildmidwestern.com>
2022-12-01 01:43:38 -0600

    [fix] eslint error

--- src/helpers/hooks/useEndpoints.ts
+++ src/helpers/hooks/useEndpoints.ts
@@ -1,70 +1,5 @@
 import type { DependencyList } from 'react';
-import { useState, useEffect, useReducer } from 'react';
-
-export interface IUseFetchWithAbortResponse {
-  data: unknown;
-  status: string;
-  error: Error | null;
-}
-
-type FetchAction =
-  | { type: 'idle'; payload: Record<string, unknown> }
-  | { type: 'fetching'; payload: Record<string, unknown> }
-  | { type: 'success'; payload: any }
-  | { type: 'error'; error: Error };
-
-const FetchReducer = (state: any, action: FetchAction) => {
-  switch (action.type) {
-    case 'idle': {
-      return { ...state, status: 'idle', error: null, payload: {} };
-    }
-    case 'fetching': {
-      return { ...state, status: 'pending', error: null, payload: {} };
-    }
-    case 'success': {
-      return { ...state, status: 'resolved', data: action.payload };
-    }
-    case 'error': {
-      return { ...state, status: 'error', error: action.error.message };
-    }
-    default: {
-      return state;
-    }
-  }
-};
-export const useFetchWithAbort = (
-  endpoint: string,
-  options?: ResponseInit,
-): IUseFetchWithAbortResponse => {
-  const [{ status, error, data }, dispatch] = useReducer(FetchReducer, initialFetchState);
-
-  useEffect(() => {
-    const abortController = new AbortController();
-    const fetchData = async () => {
-      // idle dispatch stuff
-      try {
-        const response = await fetch(endpoint, {
-          ...options,
-          signal: abortController.signal,
-        });
-        const newData = await response.json();
-        // dispatch stuff
-      } catch (error) {
-        if (error instanceof Error) {
-          if (error.name === 'AbortError') {
-            // dispatch error stuff
-          }
-        }
-      }
-    };
-    void fetchData();
-    return () => {
-      abortController.abort();
-    };
-  }, [endpoint, options]);
-
-  return { data, status, error };
-};
+import { useState, useEffect } from 'react';
 
 export const LoadingState = {
   pending: 'pending',
@@ -93,6 +28,8 @@ export const useEndpoint = <TData>(
         setData(newData);
       })
       .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.error(err);
         setLoadingState(LoadingState.error);
       });
 
