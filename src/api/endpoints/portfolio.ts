d0959fa2a2febd55b34c7c30452bacc0be6dc1aa
lzustiak <lz@buildmidwestern.com>
2022-12-13 15:47:23 -0600

    [feat] parse api responses using zod

--- src/api/endpoints/portfolio.ts
+++ src/api/endpoints/portfolio.ts
@@ -1,13 +1,18 @@
 import { apiClient } from '@/api/client';
 import type { IPortfolioData } from '@/types';
+import { portfolioSchema } from '@/schemas/portfolio.schemas';
 
-export const getPortfolioAssets = async (signal?: AbortSignal) => {
-  const response = await apiClient.get(`/portfolio/`, { signal });
-  return response.data;
-};
+export const getPortfolioAssets = async (
+  signal?: AbortSignal,
+): Promise<IPortfolioData | undefined> => {
+  try {
+    const res = await apiClient.get(`/portfolio/`, { signal });
 
-export const getPortfolioWatchlistAssets = async (signal?: AbortSignal) => {
-  const response = await apiClient.get(`/watchlist`, { signal });
+    return portfolioSchema.parse(res.data);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
 
-  return response.data;
+    return;
+  }
 };
