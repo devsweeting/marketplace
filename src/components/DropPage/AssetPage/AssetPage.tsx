b2f6ce975abee8787025f90f49d954780b42e2dc
Devin Sweeting <devinsweeting@gmail.com>
2023-03-07 11:20:11 -0600

    remove console

--- src/components/DropPage/AssetPage/AssetPage.tsx
+++ src/components/DropPage/AssetPage/AssetPage.tsx
@@ -75,8 +75,6 @@ export function AssetPage(props: AssetPageProps) {
     handleRemoveWatch,
   } = props;
 
-  console.log('AssetPage', sellOrder);
-
   const [url, setUrl] = useState('');
   const [modalState, setModalState] = useState<boolean>(false);
   const [unitsToPurchase, setUnitsToPurchase] = useState<number>(1);
